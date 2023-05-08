8e9bb40ac0c6faa30b89679687c22db05f5eb4d8
Tiago Assuncao <tiagoribeirodeassuncao@gmail.com>
2023-05-08 13:19:15 +0000

    Addding docstring

--- validate_xlsx_file.py
+++ validate_xlsx_file.py
@@ -1,25 +1,39 @@
 import openpyxl
 import re
 
-# Open the Excel file
-workbook = openpyxl.load_workbook('file.xlsx')
+def check_duplicates():
+    """Check for duplicate values in the second column of an Excel file.
 
-# Select the first worksheet
-sheet = workbook.active
+    Reads an Excel file named 'file.xlsx', selects the first worksheet, and stores the values of the second column in a list
+    after cleaning the values of any whitespace or special characters. Then, checks for duplicate values in the list and
+    prints out the rows containing the duplicates.
 
-# Store the values of the second column in a list, cleaned of whitespace and special characters
-column2 = [re.sub(r'\W+', '', str(cell.value).strip()) for cell in sheet['B']]
+    Returns:
+    None
+    """
 
-duplicates = {}
+    # Open the Excel file
+    workbook = openpyxl.load_workbook('file.xlsx')
 
-for i, value in enumerate(column2):
-    if value in column2[i+1:] and value not in duplicates.values():
-        index = column2[i+1:].index(value) + i + 1
-        duplicates[index] = value
+    # Select the first worksheet
+    sheet = workbook.active
 
-if duplicates:
-    print("The following rows contain duplicate values in the second column:")
-    for index, value in duplicates.items():
-        print(f"Row {index+1}: {value}")
-else:
-    print("There are no duplicate values in the second column of the file.")
+    # Store the values of the second column in a list, cleaned of whitespace and special characters
+    column2 = [re.sub(r'\W+', '', str(cell.value).strip()) for cell in sheet['B']]
+
+    duplicates = {}
+
+    for i, value in enumerate(column2):
+        if value in column2[i+1:] and value not in duplicates.values():
+            index = column2[i+1:].index(value) + i + 1
+            duplicates[index] = value
+
+    if duplicates:
+        print("The following rows contain duplicate values in the second column:")
+        for index, value in duplicates.items():
+            print(f"Row {index+1}: {value}")
+    else:
+        print("There are no duplicate values in the second column of the file.")
+
+
+check_duplicates()
